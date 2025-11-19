1e688e991ab678dd7ac2d98d927cd081dd01e92b
jcook3701 <jcook3701@gmail.com>
2025-11-19 01:13:47 -0800

    updates

--- {{ cookiecutter.package_name }}/build_utils/__init__.py
+++ {{ cookiecutter.package_name }}/build_utils/__init__.py
@@ -8,6 +8,6 @@ Author: Jared Cook
 Description: Initialization of Build Utilities
 """
 
-from .yaml_front_matter import add_front_matter_to_dir
+# from .yaml_front_matter import add_front_matter_to_dir
 
-__all__ = ["add_front_matter_to_dir"]
+# __all__ = ["add_front_matter_to_dir"]
