97b4a9362e1b19566e024f553c0ff24ef2f2234b
jcook3701 <jcook3701+github@gmail.com>
2025-12-16 01:10:50 -0800

    feat(header): Got license headers working.

--- {{ cookiecutter.project_slug }}/plugins/modules/test.py
+++
@@ -1,9 +0,0 @@
-{% import '.cookiecutter_includes/license_header.jinja' as license_macros with context %}
-{{- license_macros.license_header(
-  cookiecutter.license,
-  cookiecutter.author,
-  cookiecutter.project_slug,
-  file_name='test.py',
-  comment_style='python') }}
-
-import sphinx
